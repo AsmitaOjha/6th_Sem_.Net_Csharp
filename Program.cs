d4418830ed8745fb40fb216f710397f34b4963fd
Asmita Ojha <asmitaojha05@gmail.com>
2024-03-12 21:38:31 +0545

    finally basics completed

--- Program.cs
+++ Program.cs
@@ -6,7 +6,27 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Good Evening Everyone !!");
+        // Console.WriteLine("Good Evening Everyone !!");
+        // string[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+        // for (int i = 0; i < cars.Length; i++) 
+        // {
+        //     Console.WriteLine(cars[i]);
+        //     // Console.ReadLine();
+        // }
+        // Console.WriteLine("Hello Asmita");
+        // //int a = 80;
+        // //int b = 90;
+        // //Console.WriteLine(a + b);
+
+        Console.WriteLine("Enter first number: ");
+        string a = Console.ReadLine();
+        Console.WriteLine("Enter second number: ");
+        string b = Console.ReadLine();
+        int firstnumber = Convert.ToInt32(a);
+        int secondnumber = Convert.ToInt32(b);
+        int sum =firstnumber + secondnumber;
+        Console.WriteLine("The sum is : " + sum);
+        Console.ReadLine();
+            }
         }
-    }
-}
+        }
