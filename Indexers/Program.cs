e076a7a1b8a91cbaf0246fa7a9b1a7a9b4cf5b89
Asmita Ojha <asmitaojha05@gmail.com>
2024-03-15 19:36:58 +0545

    indexer

---
+++ Indexers/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class SampleCollection
+{
+    private string[] data = new string[5]; // Array to store data
+
+    // Indexer declaration
+    public string this[int index]
+    {
+        get
+        {
+            return data[index];
+        }
+        set
+        {
+            data[index] = value;
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        SampleCollection collection = new SampleCollection();
+
+        // Using the indexer to set values
+        collection[0] = "Item 1";
+        collection[1] = "Item 2";
+        collection[2] = "Item 3";
+
+        // Using the indexer to get values
+        Console.WriteLine(collection[0]); // Output: Item 1
+        Console.WriteLine(collection[1]); // Output: Item 2
+        Console.WriteLine(collection[2]); // Output: Item 3
+    }
+}
---
+++ Inheritance/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Family{
+    public string Family_Head_name, age, relation;
+    public string getInfo(){
+        Console.Write("Family_Head: ");
+        Family_Head_name = Console.ReadLine();
+        Console.Write("Age: "); // Fixed: Prompt for age separately
+        age = Console.ReadLine();
+        Console.Write("Relation: "); // Fixed: Prompt for relation separately
+        relation = Console.ReadLine();
+        return "Name: " + Family_Head_name + ", Age: " + age + ", Relation: " + relation; // Fixed: Return concatenated string
+    }
+}
+
+class Bhatt_Family : Family{
+    public string Address = "Bankhet"; //bhatt_family field
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //create a bhatt_family object
+        Bhatt_Family Hari = new Bhatt_Family();
+        
+        // Moved getInfo() call here
+        Console.WriteLine("Hari's Family Head " + Hari.getInfo());
+    }
+}
